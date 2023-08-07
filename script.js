9393b2c7ee110eaf268f3d70cc02ae88ddfd691a
Valentino Lucero <valentinolucero93gmail.com>
2023-08-07 15:24:46 -0300

    Terminando la pagina

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-const carrito = [];
+let carrito =JSON.parse(localStorage.getItem("carrito")) || [];
 let resultado = 0
+let totalCarrito = document.getElementById("total");
 
+
+//primer renderizado
+renderizarprod(Productos);
+if(carrito.length!=0){
+  for(const prod of carrito){
+    document.getElementById("tablacarrito").innerHTML +=`
+    <tr>
+      <td>${prod.id}</td>
+      <td>${prod.nombre}</td>
+      <td>${prod.precio}</td>
+    </tr>
+  `
+  resultado += prod.precio;
+  }
+
+  totalCarrito.innerHTML = "";
+
+  totalCarrito.innerHTML += `
+  Total a pagar $: ${resultado}
+  `;
+}
+
+//Renderizar los productos
 function renderizarprod(Productos){
   for (const producto of Productos){
     document.getElementById("contenidotienda").innerHTML +=`
@@ -15,8 +39,8 @@ function renderizarprod(Productos){
       </div>
     </div>
     `
-    let botones = document.getElementsByClassName("compra");
 
+    let botones = document.getElementsByClassName("compra");
 
     for (const boton of botones){
 
@@ -29,21 +53,59 @@ function renderizarprod(Productos){
   } 
 }
 
+const boton1 = document.getElementById("mayorprecio");
+const boton2 = document.getElementById("menorprecio");
+
+boton1.onclick=()=> mayormenor();
+boton2.onclick=()=> menormayor();
+
+function mayormenor(){
+  Productos.sort((b,a)=>a.precio-b.precio);
+  document.getElementById("contenidotienda").innerHTML=""
+  renderizarprod(Productos);
+}
+
+function menormayor(){
+  Productos.sort((a,b)=>a.precio-b.precio);
+  document.getElementById("contenidotienda").innerHTML=""
+  renderizarprod(Productos);
+}
 
 let tablaCarrito = document.getElementById("tablacarrito");
 
-
+resultado=0;
 function agregarACarrito(producto){
   carrito.push(producto);
-  tablacarrito.innerHTML +=`
+  tablaCarrito.innerHTML +=`
     <tr>
       <td>${producto.id}</td>
       <td>${producto.nombre}</td>
       <td>${producto.precio}</td>
     </tr>
   `;
+
+  totalCarrito.innerHTML = "";
+  resultado += producto.precio;
+  //RESULTADO
+  totalCarrito.innerHTML += `
+  Total a pagar $: ${resultado}
+  `;
+  
+  //local storage
+  localStorage.setItem("carrito",JSON.stringify(carrito));
 }
 
 
-renderizarprod(Productos);
+//boton de compra
+const btncompra = document.getElementById("pagar");
 
+btncompra.onclick = () =>{
+  alert("Gracias por tu compra!");
+
+  carrito = [];
+  tablaCarrito.innerHTML="" 
+  resultado= 0;
+  totalCarrito.innerText="Total a pagar $:";
+  localStorage.removeItem("carrito");
+
+}
